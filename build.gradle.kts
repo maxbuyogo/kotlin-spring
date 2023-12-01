5c3b174af9656176e48fee67df96be9c321b260c
maxbuyogo <144339964+maxbuyogo@users.noreply.github.com>
2023-12-01 14:50:53 +0900

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
 
 group = "io.paketo"
 version = "0.0.1-SNAPSHOT"
-java.sourceCompatibility = JavaVersion.VERSION_1_8
+java.sourceCompatibility = JavaVersion.VERSION_17
 
 repositories {
 	mavenCentral()
